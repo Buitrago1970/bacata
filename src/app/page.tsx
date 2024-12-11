f99d3c9ac88d684a09185001f79247873e48f38f
Juan Buitrago <juanbuitrago2002@outlook.com>
2024-12-10 20:25:57 -0500

    Add Tailwind CSS configuration and update global styles

--- src/app/page.tsx
+++ src/app/page.tsx
@@ -2,9 +2,9 @@
 
 export default function Home() {
   return (
-
-    <div>
-      pagina home
+    <div className="flex flex-col items-center justify-center min-h-screen bg-gray-100">
+      <h1 className="text-6xl font-bold">Hello, World!</h1>
+      <p className="text-xl mt-4">This is a Next.js app with Tailwind CSS.</p>
     </div>
-    );
+  );
 }
